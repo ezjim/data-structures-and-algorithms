803b14cf810bb676a8f814d5935356e38404f6fb
James <james.re@yahoo.com>
2020-03-13 15:30:58 -0700

    starting code challenge

---
+++ challenges/arrayMap/array-map.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const mapOne = numbers.map(item => item + 1);
+console.log(mapOne);
+
+module.exports = { mapOne };
+
+///!!!!!!!!!!! travis yml   and eslint from source code    
